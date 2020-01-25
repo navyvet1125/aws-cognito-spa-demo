9111047c3628749545b276607834da1ac61311e0
Evan Washington <enavy04@gmail.com>
2020-01-24 22:47:13 -0800

    Got demo to work

---
+++ src/app/user-info-store.js
@@ -0,0 +1,26 @@
+const state = {
+  cognitoInfo: {},
+  loggedIn: false,
+  loadingState: true,
+  errorLoadingState: false
+}
+
+function setLoggedIn(newValue) {
+  state.loggedIn = newValue;
+}
+
+function setLoggedOut() {
+  state.loggedIn = false;
+  state.cognitoInfo = {};
+}
+
+function setCognitoInfo(newValue){
+  state.cognitoInfo = newValue;
+}
+
+export default {
+  state: state,
+  setLoggedIn: setLoggedIn,
+  setLoggedOut: setLoggedOut,
+  setCognitoInfo: setCognitoInfo
+}
